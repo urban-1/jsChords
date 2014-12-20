ff6a199eefee94bdbcd7f455736793c43524c527
root <root@scampus.(none)>
2014-12-20 11:39:46 +0000

    Added jsduck docs

---
+++ scritps/fetchDoc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#
+# Assuming that is in the same lavel witht the jsChords project
+#
+JSCHORDS="../../jsChords"
+
+if [ $# -gt 0 ]; then
+    JSCHORDS=$1
+fi
+
+(cd $JSCHORDS && make doc)
+cp -r $JSCHORDS/doc/html/* ../doc
