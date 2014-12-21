3e455c2c26d1e4460e6048e805e6c8c2bc488d1f
x02210 <x02210@scampus>
2014-12-21 12:37:07 +0000

    Demo separate css + cleaning renderers... (they will become XxxUtil.js)

--- src/gui/IRenderer.js
+++ src/gui/IRenderer.js
@@ -14,12 +14,5 @@ C.IRenderer = C.Class.extend({
 })
 
 C.IRenderer.byType = function(opts){
-    var t = C.Util.objValue(opts, "type", "html");
-    
-    switch (t) {
-	case "html":
-	    return new C.RendererHtml(opts);
-	default:
-	    throw new Error({'C.IRenderer':'Renderer "'+t+'" is not known...'}) 
-    }
+
 }
