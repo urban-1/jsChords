775ed5fce1a60085fef71403bdab821ab16d9a71
x02210 <x02210@scampus>
2014-12-25 14:14:01 +0000

    Tuner: More stats...

--- mods/tunerWorker.js
+++ mods/tunerWorker.js
@@ -4,36 +4,103 @@ self.onmessage = function(event)
 	var timeseries = event.data.timeseries;
 	var test_frequencies = event.data.test_frequencies;
 	var sample_rate = event.data.sample_rate;
-	var amplitudes = compute_correlations(timeseries, test_frequencies, sample_rate);
-	self.postMessage({ "timeseries": timeseries, "frequency_amplitudes": amplitudes });
+	var data = anal(timeseries, test_frequencies, sample_rate);
+	self.postMessage(data);
 };
 
-function compute_correlations(timeseries, test_frequencies, sample_rate)
+function anal(timeseries, test_frequencies, sample_rate)
 {
 	// 2pi * frequency gives the appropriate period to sine.
 	// timeseries index / sample_rate gives the appropriate time coordinate.
 	var scale_factor = 2 * Math.PI / sample_rate;
-	var amplitudes = test_frequencies.map
-	(
-		function(f)
-		{
-			var frequency = f.frequency;
-
-			// Represent a complex number as a length-2 array [ real, imaginary ].
-			var accumulator = [ 0, 0 ];
-			for (var t = 0; t < timeseries.length; t++)
-			{
-			    if (Math.abs(timeseries[t])<0.05)
-				continue;
-			    
-			    accumulator[0] += timeseries[t] * Math.cos(scale_factor * frequency * t);
-			    accumulator[1] += timeseries[t] * Math.sin(scale_factor * frequency * t);
-			}
-
-			return accumulator;
-		}
-	);
 	
+	/**
+	 * Calculate as much stats here as possible
+	 */
+	var flen = test_frequencies.length;
+	var tlen = timeseries.length;
+	var amplitudes = [];
+	var magnitudes = [];
+	
+	// frequency statistics
+	var fstats = {
+	    max: -100000,
+	    maxIdx: -1,
+	    avgF: 0,
+	    avgM: 0
+	};
+	
+	var tstats = {
+	    max: -100000,
+	    min: 100000,
+	    amp: -1
+	};
+	var sum_mag = 0;
+	var timeDone = false;
+	
+	// For each frequency and time
+	for (var f=0; f<flen; f++){
+	    
+	    // Current F
+	    var frequency = test_frequencies[f].frequency;
+	    
+	    // Represent a complex number as a length-2 array [ real, imaginary ].
+	    var accumulator = [ 0, 0 ];
+	    
+	    for (var t=0; t<tlen; t++){
+		
+		// Time stats
+		if (!timeDone) {
+		    if (timeseries[t]>tstats.max) tstats.max=timeseries[t];
+		    if (timeseries[t]<tstats.min) tstats.min=timeseries[t];
+		}
+		
+// 		if (Math.abs(timeseries[t])<0.05)
+// 		    continue;
+		
+		accumulator[0] += timeseries[t] * Math.cos(scale_factor * frequency * t);
+		accumulator[1] += timeseries[t] * Math.sin(scale_factor * frequency * t);
+		
+		
+	    }
+	    
+	    amplitudes[f] = accumulator.slice();
+	    
+	    // Compute the (squared) magnitudes of the complex amplitudes for each
+	    // test frequency. 
+	    magnitudes[f] = (accumulator[0]*accumulator[0] + accumulator[1]*accumulator[1]);
+	    
+	    
+	    // Stats 
+	    if (magnitudes[f]>fstats.max) {
+		fstats.max = magnitudes[f];
+		fstats.maxIdx = f;
+	    }
+	    
+	    
+	    sum_mag+=magnitudes[f];
+	    fstats.avgF+=test_frequencies[f].frequency * magnitudes[f];
+	}
+	
+	// Final stats
+	fstats.avgF = fstats.avgF/sum_mag;
+	
+	// Compute the average magnitude. We'll only pay attention to frequencies
+	// with magnitudes significantly above average.
+	fstats.avgM = sum_mag/magnitudes.length;
+	
+	// Analog amplitude
+	tstats.amp = tstats.max-tstats.min;
 
-	return amplitudes;
+	return 	{ 
+		    timeseries: timeseries, 
+		    frequency: {
+			amplitudes: amplitudes,
+			magnitudes: magnitudes,
+			stats: fstats
+		    },
+		    time: {
+			stats: tstats
+		    }
+		};
 }
